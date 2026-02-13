fa5aa58bedb45b173f06d5805c55246a9748e599
unknown <stephenmcnaughton01@gmail.com>
2026-02-13 09:50:54 +0000

    initial commit

---
+++ kafka-basics/src/main/java/org/example/ProducerDemo.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Properties properties  = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        ProducerRecord<String, String> producerRecord =
+                new ProducerRecord<>("demo_java", "hello world");
+
+        //async
+        producer.send(producerRecord);
+
+        //syn
+        producer.flush();
+
+        //flush and close
+        producer.close();
+    }
+}
